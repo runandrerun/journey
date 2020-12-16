c471ef225d30c1ad1f0571f6ed0341c3099b7e65
runandrerun <the.asantiagojr@gmail.com>
2020-12-15 20:13:57 -0500

    Minor copy updates.

--- src/containers/feature/Feature.container.jsx
+++ src/containers/feature/Feature.container.jsx
@@ -5,8 +5,8 @@ export default function FeatureContainer({children}) {
   return (
     <Feature>
       <Feature.HeadingsWrap>
-      <Feature.Title>penpals.</Feature.Title>
-      <Feature.Subtitle>discover, write, and grow</Feature.Subtitle>
+      <Feature.Title>journey.</Feature.Title>
+      <Feature.Subtitle>wake, explore, and discover.</Feature.Subtitle>
       </Feature.HeadingsWrap>
       {children}
     </Feature>
